da08705c0cef26e12e692c974854956ea0088fc6
Matthew McCorry <matthew.mccorry@creative-technologies.co.uk>
2019-03-14 18:19:50 +0000

    added random down keys for more convincing code

--- CruiseControl.ino
+++ CruiseControl.ino
@@ -4,10 +4,16 @@ void setup() {
 }
 
 void loop() {             
-    DigiKeyboard.sendKeyStroke(KEY_SPACE,MOD_CONTROL_LEFT);
-    DigiKeyboard.delay(100);
+    DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_CONTROL_LEFT);
+    DigiKeyboard.delay(200);
+    int downs = random(5);
+    for (int i = 0; i < downs; i++)
+    {
+      DigiKeyboard.sendKeyStroke(81);
+      DigiKeyboard.delay(50);
+    }
     DigiKeyboard.sendKeyStroke(KEY_ENTER);
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(50);
     DigiKeyboard.write(".");
-    DigiKeyboard.delay(1000);
+    DigiKeyboard.delay(50);
 }
